068b7e87b00285d003446c4bdcca4bb6f8c15242
EC2 Default User <ec2-user@ip-172-31-29-13.ap-southeast-1.compute.internal>
2022-07-21 02:17:48 +0000

    First commit

---
+++ test_lab1.py
@@ -0,0 +1,11 @@
+def increment_by_one(i):
+    return i + 1
+    
+def test_answer_increment():
+    assert increment_by_one(1) == 2
+
+def decrement_by_one(i):
+    return i - 1
+
+def test_answer_decrement():
+    assert decrement_by_one(2) == 1
